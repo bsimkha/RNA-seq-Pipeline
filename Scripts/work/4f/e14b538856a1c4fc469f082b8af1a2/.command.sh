3d798e0ba0d194a2662d48413d3c5257bd057a83
Bibhu Simkhada <bsimkha@g.clemson.edu>
2026-03-29 16:32:08 -0400

    Cleanup

--- Scripts/work/4f/e14b538856a1c4fc469f082b8af1a2/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-fastp       -i sample_R2_1.fq.gz       -I sample_R2_2.fq.gz       -o sample_R2_R1.trim.fq.gz       -O sample_R2_R2.trim.fq.gz       -j sample_R2.fastp.json       -h sample_R2.fastp.html       --detect_adapter_for_pe       -w 4
